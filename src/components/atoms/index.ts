0b840235a6ad81354af971967444619e2a1f35d2
deepeshjaindj <jaind1616@gmail.com>
2025-04-20 14:50:47 +0530

    Background and Floating Dock UI Components

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,5 @@
 export { default as AuroraBackground } from "./AuroraBackground";
+export { default as BackgroundBeams } from "./BackgroundBeams";
+export { default as BackgroundBeamsCollide } from "./BackgroundBeamsCollide";
+export { default as FloatingDock } from "./FloatingDock";
 export { default as ShiningText } from "./ShiningText";
