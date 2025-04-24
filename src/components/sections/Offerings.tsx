9163ceadff3ae235c371b269ca19e0e6e7a2be9f
deepeshjaindj <jaind1616@gmail.com>
2025-04-25 03:34:43 +0530

    Dark item cleanup

--- src/components/sections/Offerings.tsx
+++ src/components/sections/Offerings.tsx
@@ -208,38 +208,38 @@ const Offerings = () => {
           initial="initial"
           animate="animate"
           whileHover="hover"
-          className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2"
+          className="flex flex-1 w-full h-full min-h-[6rem] bg-dot-black/[0.2] flex-row space-x-2"
         >
           <motion.div
             variants={FIRST_CARD_ANIMATION_OFFERINGS}
-            className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+            className="h-full w-1/3 rounded-2xl bg-white p-4 border border-neutral-200 flex flex-col items-center justify-center"
           >
             <FaFigma className="rounded-full h-10 w-10 p-2 bg-red-100 text-red-600 border border-red-500" />
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
               Pixel Perfect UI
             </p>
-            <p className="border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-red-500 bg-red-100 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
               Precision
             </p>
           </motion.div>
-          <motion.div className="h-full relative w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
+          <motion.div className="h-full relative w-1/3 rounded-2xl bg-white p-4 border border-neutral-200 flex flex-col items-center justify-center">
             <PiLego className="rounded-full h-10 w-10 p-2 bg-green-100 text-green-600 border border-green-500" />
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
               Lego-Like Structure
             </p>
-            <p className="border border-green-500 bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-green-500 bg-green-100 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
               Reusable
             </p>
           </motion.div>
           <motion.div
             variants={SECOND_CARD_ANIMATION_OFFERINGS}
-            className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+            className="h-full w-1/3 rounded-2xl bg-white p-4 border border-neutral-200 flex flex-col items-center justify-center"
           >
             <AiOutlineApi className="rounded-full h-10 w-10 p-2 bg-orange-100 text-orange-600 border border-orange-500" />
             <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
               Always In the Loop
             </p>
-            <p className="border border-orange-500 bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
+            <p className="border border-orange-500 bg-orange-100 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
               Async-friendly
             </p>
           </motion.div>
@@ -249,7 +249,7 @@ const Offerings = () => {
   ];
 
   return (
-    <div className="min-h-dvh flex items-center justify-center bg-gradient-to-b from-primaryYellow/20 to-white pt-12 font-sans">
+    <div className="min-h-dvh flex items-center justify-center bg-gradient-to-b from-primaryYellow/20 to-white pt-16 pb-8 font-sans">
       <BentoGrid className="w-full max-w-7xl px-4 sm:px-6 lg:px-10 xl:px-16">
         {FEATURES.map((feature, idx) => (
           <BentoCard
