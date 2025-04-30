81dd33b11cdb44cbc5dffb101e33fe029e11a2ca
deepeshjaindj <jaind1616@gmail.com>
2025-05-01 04:15:02 +0530

    Added tailwind safelist

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "border-teal-500",
+    "bg-teal-100",
+    "text-teal-700",
+    "border-amber-500",
+    "bg-amber-100",
+    "text-amber-700",
+    "border-purple-500",
+    "bg-purple-100",
+    "text-purple-700",
+    "border-pink-500",
+    "bg-pink-100",
+    "text-pink-700",
+  ],
+};
